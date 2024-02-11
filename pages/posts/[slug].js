7ec391a3aacdbce8e99edc8b225fa27b1ce5a800
rbeyzas <rukiyebeyzasarikaya@hotmail.com>
2024-02-11 10:35:15 +0300

    pages created

---
+++ pages/posts/[slug].js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PostDetailPage() {
+  return <div>Post Detail Page</div>;
+}
+
+export default PostDetailPage;
---
+++ pages/posts/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AllPostsPage() {
+  return <div>All Posts Page</div>;
+}
+
+export default AllPostsPage;
