f3d3b29934e5bc87e663d2062238cd21c528a21b
rbeyzas <rukiyebeyzasarikaya@hotmail.com>
2024-04-12 20:44:13 +0300

    post content and post header

---
+++ components/Posts/PostDetail/PostContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostHeader from './PostHeader';
+
+function PostContent() {
+  return (
+    <article>
+      <PostHeader />
+      Content
+    </article>
+  );
+}
+
+export default PostContent;
